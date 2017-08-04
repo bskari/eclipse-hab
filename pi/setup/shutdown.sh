6f45607838bec7beb9751aaae56f72360d40723d
Brandon Skari <brandon@skari.org>
2017-08-03 22:44:13 -0600

    Even shutdown -k blocks SSH login attempts, so don't do that

--- pi/setup/shutdown.sh
+++ pi/setup/shutdown.sh
@@ -5,7 +5,6 @@ if [ "${uptime_seconds}" -lt 300 ] ;
 then
 	# Sleep, because shutdown blocks new logins from SSH within 5 minutes of
 	# shutdown, which is annoying
-	sudo shutdown -k +5
 	sleep 240
 fi
 sudo shutdown -h +1
